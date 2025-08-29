71fa28e41de7d1bdf0c8103d09c18e03409d8177
natee <nateehunter8@gmail.com>
2025-08-29 09:27:29 +0700

    preform migration prisma model

---
+++ src/server.ts
@@ -0,0 +1,31 @@
+// src/app.ts
+import Fastify from "fastify";
+import cookie from "@fastify/cookie";
+import jwt from "@fastify/jwt";
+import rateLimit from "@fastify/rate-limit";
+import cors from "@fastify/cors";
+import { prismaPlugin } from "./plugins/prisma";
+
+
+// env config must be defined before use
+
+export async function buildApp() {
+  const app = Fastify({ logger: true });
+
+  await app.register(rateLimit, { max: 100, timeWindow: "1 minute" });
+  
+  await app.register(cors, {
+    origin: (origin, cb) => cb(null, true), // ปรับ allow-list ในโปรดักชัน
+    credentials: true,
+  });
+  await app.register(cookie, { hook: "onRequest" });
+
+  await app.register(prismaPlugin);
+
+  // routes
+
+  // hook ตรวจ access token
+
+
+  return app;
+}
